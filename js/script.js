e58cf465875e303d856ac11f87cb2f77f00c4c35
Pavel <p.filippov2.20@gmail.com>
2023-05-09 22:41:42 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,15 @@
+'use strict';
+// alert('Hi!');
+// const result = confirm('Are you alive?'); 
+// console.log(result);
+// const answer = prompt('как дела?', 'x...');
+const answer = [];
+answer[0] = prompt('Как жизнь?');
+console.log(answer);
+answer[1] = prompt('Что нового?');
+console.log(answer);
+answer[2] = prompt('Как дела?');
+console.log(answer);
+console.log(typeof(answer));
+
+
